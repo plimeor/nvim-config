af82372457f85e621beaba77a55749353a2f5a96
Plimeor <plimeor@gmail.com>
2023-07-09 21:22:30 +0800

    Add debug config for rust via CodeLLDB

--- lua/plimeor/lsp/rust-tools.lua
+++ lua/plimeor/lsp/rust-tools.lua
@@ -1,3 +1,18 @@
+-- Update this path
+local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.9.2/"
+local codelldb_path = extension_path .. "adapter/codelldb"
+local liblldb_path = extension_path .. "lldb/lib/liblldb"
+local this_os = vim.loop.os_uname().sysname
+
+-- The path in windows is different
+if this_os:find("Windows") then
+	codelldb_path = extension_path .. "adapter\\codelldb.exe"
+	liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+else
+	-- The liblldb extension is .so for linux and .dylib for macOS
+	liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+end
+
 return {
 	"simrat39/rust-tools.nvim",
 	dependencies = {
@@ -16,14 +31,15 @@ return {
 				on_attach = function(client, bufnr)
 					lsp_utils.on_attach(client, bufnr)
 
-					vim.keymap.set(
-						"n",
-						"J",
-						require("rust-tools").join_lines.join_lines,
-						{ buffer = bufnr, desc = "Rust Join Lines" }
-					)
+					vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+
+					vim.keymap.set("n", "J", rt.join_lines.join_lines, { buffer = bufnr, desc = "Rust Join Lines" })
 				end,
 				capabilities = lsp_utils.get_lsp_capabilities(),
+			},
+
+			dap = {
+				adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
 			},
 		})
 	end,
