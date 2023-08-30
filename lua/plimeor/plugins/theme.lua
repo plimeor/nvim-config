14c6b9a56e2d6391b768e817b89635f6591b9fc6
Plimeor <plimeor@gmail.com>
2023-08-30 22:50:17 +0800

    Set transparent background

--- lua/plimeor/plugins/theme.lua
+++ lua/plimeor/plugins/theme.lua
@@ -6,6 +6,7 @@ return {
 	config = function()
 		require("tokyonight").setup({
 			style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+			transparent = true,
 		})
 		vim.cmd([[colorscheme tokyonight]])
 	end,
